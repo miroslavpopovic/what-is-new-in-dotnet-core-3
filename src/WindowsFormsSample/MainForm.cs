07337e9266d73b72de3860594f265c9436d59cc4
Miroslav Popovic <miroslav.popovic@gmail.com>
2019-03-13 22:01:02 +0100

    Add Windows Forms and WPF samples

---
+++ src/WindowsFormsSample/MainForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsSample
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
