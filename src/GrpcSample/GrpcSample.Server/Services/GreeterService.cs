9f5629caed526bd9b502a7dfb8e586771d7ae766
Miroslav Popovic <miroslav.popovic@gmail.com>
2019-03-13 23:28:49 +0100

    Add gRPC sample

---
+++ src/GrpcSample/GrpcSample.Server/Services/GreeterService.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Greet;
+using Grpc.Core;
+
+namespace GrpcSample
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
