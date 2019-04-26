d253c17f93c1fb2e7b99e53305ab3946f4296138
Miroslav Popovic <miroslav.popovic@gmail.com>
2019-04-26 23:54:08 +0200

    Add new Grpc server and client sample using Preview 4

---
+++ src/GrpcSample.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Greet;
+using Grpc.Core;
+
+namespace GrpcSample.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number here must match the port of the gRPC server
+            var channel = new Channel("localhost:50051", ChannelCredentials.Insecure);
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            await channel.ShutdownAsync();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
